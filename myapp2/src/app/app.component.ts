863d693b02b921b0a6536cd7795ed1f86357c41a
cklgy88 <cklgy88@gmail.com>
2026-01-28 17:31:58 +0800

    0128練習

--- myapp2/src/app/app.component.ts
+++ myapp2/src/app/app.component.ts
@@ -12,12 +12,19 @@ enum apiStatus {
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-test(ms:string){
-  alert(ms+"超屌");
-  this.test2();
-}
+  res!:any;
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
 
-test2(ms="林老師"){
-alert(ms+"卡好")
-}
+    let jsonData={
+      "name":"rte",
+      "lev":"87",
+      "test":"我去"
+    }
+    this.res=jsonData
+    console.log(this.res.name);
+    
+
+    }
 }
