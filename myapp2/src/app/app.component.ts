04b6fbb7715d8fb74ca6c8976f40bb3bbe96e35a
cklgy88 <cklgy88@gmail.com>
2026-01-29 17:29:24 +0800

    0129練習

--- myapp2/src/app/app.component.ts
+++ myapp2/src/app/app.component.ts
@@ -12,19 +12,51 @@ enum apiStatus {
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  res!:any;
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    //4
+    // let data = [
+    //   {
+    //     name: '小王',
+    //     age: 10,
+    //   },
+    //   {
+    //     name: '大名',
+    //     age: 87,
+    //   },
+    //   {
+    //     name: '大王',
+    //     age: 52,
+    //   },
+    // ];
 
-    let jsonData={
-      "name":"rte",
-      "lev":"87",
-      "test":"我去"
-    }
-    this.res=jsonData
-    console.log(this.res.name);
-    
+    // data.forEach((item,index,array) => {
+    //   if(data[index].name=='小王'){
+    //     data[index].age = 18;
+    //   }
+    //     console.log(data[)
 
+    // });
+    //5
+    let data = [4, 6, 1, 2, 7, 9, 12, 15, 13];
+    let newarr = data[0];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i] < newarr) {
+        newarr = data[i];
+      }
     }
+    console.log(newarr);
+
+    //6
+    // let st = '你好我是ALLEN';
+
+    // for(let i =st.length ; i>0 ; i--){
+    //   console.log(i)
+    //   console.log(st.slice(i-1,i))
+    // }
+    //7
+    // let st = '你好我是ALLEN';
+    // let str=st.slice(2)
+    // let s=st.slice(0,2)
+    // console.log(str+","+s)
+  }
 }
