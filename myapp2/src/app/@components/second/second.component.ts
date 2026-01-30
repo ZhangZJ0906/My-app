7bcd3931e0d002d0a02147bc7bbe5d8647cef4d7
cklgy88 <cklgy88@gmail.com>
2026-01-30 17:29:55 +0800

    回家做

--- myapp2/src/app/@components/second/second.component.ts
+++ myapp2/src/app/@components/second/second.component.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-second',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './second.component.html',
-  styleUrl: './second.component.scss'
+  styleUrl: './second.component.scss',
 })
 export class SecondComponent {
+  level: number = 1;
+  attack: number = 10;
+  defeat: number = 10;
+  editlevel!: any;
 
+  lossLevel() {
+    if (this.level < 2) {
+      this.level = 1;
+      alert("我去不能小於1")
+    }
+    this.level -= 1;
+    this.attack -= 3;
+    this.defeat -= 2;
+  }
+  upLevel() {
+    this.level += 1;
+    this.attack += 3;
+    this.defeat += 2;
+  }
+  edit(newLevel: number) {
+    this.editlevel='';
+    let levels = newLevel - this.level;
+    this.level = newLevel;
+    this.attack += levels * 3;
+    this.defeat += levels * 2;
+  }
+  reset() {
+    this.attack = 10;
+    this.defeat = 10;
+    this.level = 1;
+  }
 }
