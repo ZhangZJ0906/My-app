cab7fa9ad803f775847ded9a424b6b84e6b6dbea
cklgy88 <cklgy88@gmail.com>
2026-02-05 15:50:33 +0800

    0205 API格式整理

--- myapp2/src/app/@components/apidemo/apidemo.component.ts
+++ myapp2/src/app/@components/apidemo/apidemo.component.ts
@@ -1,29 +1,72 @@
 import { Component } from '@angular/core';
 import { HttpClientService } from '../../@services/http-client.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-apidemo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './apidemo.component.html',
   styleUrl: './apidemo.component.scss',
 })
 export class ApidemoComponent {
   constructor(private http: HttpClientService) {}
-  testData!:any;
-  city!:string;
-  area!:any;
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.http.getApi(
-      'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-065?Authorization=CWA-C947A3E5-F386-4189-B35D-7A5DAEB29BF7&limit=3&offset=3'
-    ).subscribe((res:any)=>{
-      let result = res.records.Locations;
-      this.city = result[0].LocationsName;
-      this.area = result[0].Location;
-      console.log(result);
-      console.log(this.area)
+  oneHourData!: any;
+  threeHourData!: any;
+  city!: string;
+  area!: any[];
+  location!: any;
+  choseArea!: any;
+  data!: any[];
 
-    });
+  areaChange(change: string) {
+    this.data = this.location.find((element: any) => element.LocationName === change);
+    this.group(this.data);
+    // console.log('teas',this.data)
+  }
+
+  group(data: any) {
+    let test = [];
+    let threetest = [];
+    for (let i = 0; i < data.WeatherElement[0].Time.length; i++) {
+      let item = {
+        time: data.WeatherElement[0].Time[i].DataTime, //抓共同時間
+        temperature: data.WeatherElement[0].Time[i].ElementValue[0].Temperature, //溫度
+        apparentTemperatureName: data.WeatherElement[3].ElementName, //體感溫度
+        apparentTemperature:
+          data.WeatherElement[3].Time[i].ElementValue[0].ApparentTemperature,
+      };
+      test.push(item);
+    }
+    for (let i = 0; i < data.WeatherElement[8].Time.length; i++) {
+      let item = {
+        startTime: data.WeatherElement[8].Time[i].StartTime,
+        endTime: data.WeatherElement[8].Time[i].EndTime,
+        weather: data.WeatherElement[8].Time[i].ElementValue[0].Weather,
+        weatherDes:
+          data.WeatherElement[9].Time[i].ElementValue[0].WeatherDescription,
+      };
+      threetest.push(item);
+    }
+    // console.table(threetest);
+    this.oneHourData = test;
+    this.threeHourData = threetest;
+
+    // console.table(test);
+  }
+  ngOnInit(): void {
+    this.http
+      .getApi(
+        'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-065?Authorization=CWA-C947A3E5-F386-4189-B35D-7A5DAEB29BF7&limit=3&offset=3',
+      )
+      .subscribe((res: any) => {
+        let result = res.records.Locations[0];
+        this.city = result.LocationsName;
+        this.location = result.Location;
+        this.area = result.Location.map((test: any) => test.LocationName);
+        this.choseArea = this.area[0];//預設第一個
+        this.areaChange(this.choseArea);//直接顯示
+        // console.log(this.oneHourData);
+        // console.log(this.threeHourData);
+      });
   }
 }
